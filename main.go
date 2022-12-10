502064f916032199f7cf47cde0420d461ae96752
husseinmo96 <95226088+husseinmo96@users.noreply.github.com>
2022-12-10 12:05:47 +0200

    update

--- main.go
+++ main.go
@@ -12,8 +12,13 @@ func main() {
 	remainingTickets := 50
 
 	fmt.Printf("Welcome to %v booking system\n", color.BlueString(conferenceName))
-	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	fmt.Printf(
+		"We have total of %v tickets and %v are still available\n",
+		color.BlueString(fmt.Sprint(conferenceTickets)),
+		color.GreenString(fmt.Sprint(remainingTickets)),
+	)
+
+	color.New(color.Bold, color.FgGreen).Println("Get your tickets here to attend")
 
 	var userName string
 	// ask for username
