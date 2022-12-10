eb51d0eb8ae2e07fa6a717c47ceb318d033989e1
husseinmo96 <95226088+husseinmo96@users.noreply.github.com>
2022-12-10 11:25:13 +0200

    update

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
 
 func main() {
 	const conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	remainingTickets := 50
 
-	fmt.Printf("Welcome to %v booking system\n", conferenceName)
+	fmt.Printf("Welcome to %v booking system\n", color.BlueString(conferenceName))
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
